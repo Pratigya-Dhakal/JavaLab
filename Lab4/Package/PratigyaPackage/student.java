0d124049c8b7d5ba11c3680eb0e808b5f1032d5a
Pratigya-Dhakal <pratigyadhakal4444@gmail.com>
2020-03-06 14:52:22 +0545

    Files addes later

---
+++ Lab4/Package/PratigyaPackage/student.java
@@ -0,0 +1,16 @@
+package pratigya;
+
+public class students{
+	int roll;
+	String address;
+
+		public students(int r,String a){
+			roll=r;
+			address=a;
+		}
+		public void display(){
+			System.out.println("Address="+address);
+			System.out.println("Roll number ="+roll);
+		} 
+} 
+© 2020 GitHub, Inc.
---
+++ Lab4/Package/demoPackage.java
@@ -0,0 +1,7 @@
+import pratigya.students;
+class PackageDemo{
+	public static void main(String[] args) {
+		students obj1=new students (7,"aaa");
+		obj1.display();
+	}
+}
